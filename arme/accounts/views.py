24c66da820a875fa895cbd3d9100d37931c137c7
ombasa <ombasaherman@gmail.com>
2023-05-23 16:00:32 +0300

    register and login templates

--- arme/accounts/views.py
+++ arme/accounts/views.py
@@ -24,12 +24,12 @@ def register(request):
           messages.error(request, 'That email is being used')
           return redirect('register')
         else:
-          # Looks good
+
           user = User.objects.create_user(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
-          # Login after register
-          # auth.login(request, user)
-          # messages.success(request, 'You are now logged in')
-          # return redirect('index')
+          #Login after register
+          auth.login(request, user)
+          messages.success(request, 'You are now logged in')
+          return redirect('index')
           user.save()
           messages.success(request, 'You are now registered and can log in')
           return redirect('login')
